9238da9e653688ad1c7583bce0aec22bd0d07ae4
ibruceprasad <ibruceprasad@gmail.com>
2024-11-22 14:12:44 +1100

    Changed exception middleware to global exceptionhandler

---
+++ backend/Library.Api/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,63 @@
+﻿/*using library___api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace library___api.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (ApplicationException ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            var response = context.Response;
+            var errorResponse = new ErrorResponseMessage();
+
+            switch (exception)
+            {
+                case ApplicationException ex:
+
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    errorResponse.ErrorMessage = ex?.Message ?? "Application Exception";
+                    break;
+
+                default:
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    errorResponse.ErrorMessage = exception?.Message ?? "Internal Server Error";
+                    break;
+            }
+            _logger.LogError(exception?.Message);
+            var result = JsonSerializer.Serialize(errorResponse);
+            await context.Response.WriteAsync(result);
+        }
+    }
+
+
+}
+*/
---
+++ backend/Library.Api/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,53 @@
+﻿using library___api.Models;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace library___api.Middleware
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception, "An unhandled exception occurred.");
+            ProblemDetails problemDetails;
+            httpContext.Response.ContentType = "application/json";
+
+            switch (exception)
+            {
+                case ApplicationException ex:
+                    problemDetails = new ProblemDetails()
+                    {
+                        Title = "An error occurred",
+                        Status = StatusCodes.Status500InternalServerError,
+                        Detail = ex?.Message ?? "Application Exception"
+                    };
+                    break;
+
+                default:
+                    problemDetails = new ProblemDetails()
+                    {
+                        Title = "An error occurred",
+                        Status = StatusCodes.Status500InternalServerError,
+                        Detail = exception?.Message ?? "Application Exception"
+                    };
+                    break;
+            }
+            
+            httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            return true;
+            //return new ValueTask<bool>(true);
+        }
+    }
+}
